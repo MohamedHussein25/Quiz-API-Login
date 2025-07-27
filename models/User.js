4ace1d72db7a214f8c3ceca9e47b8d75fe4b1a7e
Xsl-007233 <li1652280517@gmail.com>
2025-07-27 19:39:52 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
-  password: String
+  password: String,
+  maxScore: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('User', userSchema);
